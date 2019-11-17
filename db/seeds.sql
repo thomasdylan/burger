33a802d39b250c7e48e97cf1ddcbf9a6745afa2d
Dylan Thomas <thomasdylan76@gmail.com>
2019-11-17 15:12:21 -0700

    Added seeds.  Form posting needs fixing.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
 INSERT INTO burgers (burger_name) VALUES ("The Bleeder");
+INSERT INTO burgers (burger_name) VALUES ("Breath Enhancer");
+INSERT INTO burgers (burger_name) VALUES ("The Pigpen"); 
+INSERT INTO burgers (burger_name) VALUES ("Ring of Fire"); 
+INSERT INTO burgers (burger_name) VALUES ("Bear Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Atomic Burger");
