2b69f7014e0bc9b8503ffdc54570c9034b4eed20
Dylan Thomas <thomasdylan76@gmail.com>
2019-11-17 17:51:42 -0700

    Package.json did not have a start.

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Casper52112!",
+    password: "root",
     database: "burger_db"
 });
 
